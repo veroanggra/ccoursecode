5ad3eefc3e0c6d607e45298495bb219b50bfe4a9
veroanggra <veronicaputri497@gmail.com>
2020-11-27 16:34:47 +0700

    first commit

---
+++ variable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char namaKarakter[] = "juminten";
+    int umurKarater = 12;
+
+    printf ("Skenario %s\n", namaKarakter);
+    printf("Ada seorang anak perempuan namanya %s\n", namaKarakter);
+    printf("Umur anak perempuan itu %d tahun\n", umurKarater);
+    printf("Dia sangat menyukai nama %s\n", namaKarakter);
+    printf("Tapi dia tidak suka umurnya baru %d tahun", umurKarater);
+    return 0;
+}
